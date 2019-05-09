2741b2ce2eaa594ac068223e4dd3d86ee8e87705
ledPC <lucascassan@live.com>
2019-05-09 15:27:10 -0300

    share

---
+++ js/fbshare.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2227084750941616',
+    xfbml      : true,
+    version    : 'v3.3'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+function shareonFB(){
+FB.ui({
+method: 'share',
+href: 'http://www.lucascassan.github.io/bttc/',
+}, function(response){});
+
+}
