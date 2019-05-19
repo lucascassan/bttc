9f5d70525ab30d8e309edd6b4de912d7a094238e
cassan <lucascassan@live.com>
2019-05-19 11:17:24 -0300

    Update fbshare.js

--- js/fbshare.js
+++ js/fbshare.js
@@ -23,7 +23,7 @@ FB.ui({
     description: 'BACK TO THE CASSOSHI • COMING SOON • 29.06.2019',
     link: 'http://lucascassan.github.io/bttc/',
     picture: 'https://i.imgur.com/fHX8cL7.png',
-    href: 'http://lucascassan.github.io/bttc/',
+  //  href: 'http://lucascassan.github.io/bttc/',
 
 }, function(response){});
 
