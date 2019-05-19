9712f878e0209afe661b156a75e9868580937b85
cassan <lucascassan@live.com>
2019-05-19 11:45:23 -0300

    a

--- js/fbshare.js
+++ js/fbshare.js
@@ -1,50 +1,41 @@
-// this loads the Facebook API
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2227084750941616',
+    xfbml      : true,
+    version    : 'v3.3'
+  });
+  FB.AppEvents.logPageView();
+};
 
-    window.fbAsyncInit = function () {
-        var appId = '2227084750941616';
-        FB.init({
-            appId: appId,
-            xfbml: true,
-            version: 'v2.9'
-        });
-    };
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
-    // FB Share with custom OG data.
-    (function($) {
+function shareonFB(){
+  // FB.ui({
+  //     display: 'popup',
+  //     method: 'feed',
+  //     name: '150 pontos',
+  //     caption: 'teste',
+  //     description: 'COMING SOON - 29.06.2019',
+  //     link: 'http://lucascassan.github.io/bttc/',
+  //     picture: 'https://i.imgur.com/fHX8cL7.png'//,
+  //     //href: 'http://lucascassan.github.io/bttc/'
 
-        $('.fb_share_btn').on('click', function (event) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
+  var obj = {
+    method:'share_open_graph',
+    display: 'popup',
+    redirect_uri: 'http://lucascassan.github.io/bttc/',
+    description: "$ Pontos",
+    picture: 'https://i.imgur.com/WGeBNZq.png',
+    name: 'BACK TO THE CASSOSHI'
+  };
+  FB.ui(obj);
 
-                // Dynamically gather and set the FB share data.
-                var FBDesc      = 'Your custom description';
-                var FBTitle     = 'Your custom title';
-                var FBLink      = 'http://lucascassan.github.io/bttc/';
-                var FBPic       = 'https://i.imgur.com/fHX8cL7.png';
 
-                // Open FB share popup
-                FB.ui({
-                    method: 'share_open_graph',
-                    action_type: 'og.shares',
-                    action_properties: JSON.stringify({
-                        object: {
-                            'og:url': FBLink,
-                            'og:title': FBTitle,
-                            'og:description': FBDesc,
-                            'og:image': FBPic
-                        }
-                    })
-                },
-                function (response) {
-                // Action after response
-                })
-        })
 
-    })( jQuery );
+}
