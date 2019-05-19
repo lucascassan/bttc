33577b3105f58d6a498b132189d2b25c2606e299
cassan <lucascassan@live.com>
2019-05-19 11:22:38 -0300

    Update fbshare.js

--- js/fbshare.js
+++ js/fbshare.js
@@ -21,9 +21,9 @@ FB.ui({
     method: 'share',
     title: '150 pontos',
     description: 'COMING SOON - 29.06.2019',
-    //link: 'http://lucascassan.github.io/bttc/',
-    picture: 'https://i.imgur.com/fHX8cL7.png',
-    href: 'http://lucascassan.github.io/bttc/'
+    link: 'http://lucascassan.github.io/bttc/',
+    picture: 'https://i.imgur.com/fHX8cL7.png'//,
+    //href: 'http://lucascassan.github.io/bttc/'
 
 }, function(response){});
 
